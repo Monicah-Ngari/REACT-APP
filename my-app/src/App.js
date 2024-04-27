c7660fa548dbaaa711641037af4c90f943a2dffc
Monicah-Ngari <wakarimangari@gmail.com>
2024-04-27 05:43:09 +0300

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import "./index.css";
+import Weather from "./Weather.js";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Weather />
+      <p>
+        Open-source code, by Monicah Ngari from{" "}
+        <a href="https://www.shecodes.io/demos/react">She Codes</a>
+      </p>
+    </div>
+  );
+}
